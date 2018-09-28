c449d51621e84e5bb3c73c0ff3375f53349a0892
Juan Luis Almazo <jluisalmazo@hotmail.com>
2018-09-27 20:49:46 -0500

    header-bar component was added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule
